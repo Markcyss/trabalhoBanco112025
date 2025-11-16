fc50cb27ad2460b303077111dc9a5504ac6f569f
Markcyss <marsoucar28@gmail.com>
2025-11-15 21:28:50 -0300

    banco att

--- data/banco.sql
+++ data/banco.sql
@@ -4,8 +4,19 @@ use trabalhobancobim4;
 create table usuario (
 	id int primary key auto_increment,
     nome varchar(255),
-    email varchar(100),
+    email varchar(100) unique not null,
     senha varchar(255),
     role varchar(100)
 );
 
+select*from usuario;
+
+insert into usuario (nome, email, senha, role)
+	values
+    ("Amanda", "amanda@gmail.com", MD5("4321"), "user"),
+    ("Ana", "ana@gmail.com", MD5("2123"), "user"),
+    ("Gabriel", "gabriel@gmail.com", MD5("4344"), "user"),
+    ("Gabi", "gabi@gmail.com", MD5("5678"), "admin"),
+    ("Thiago", "thiago@gmail.com", MD5("9090"), "user"),
+    ("Jonas", "jonas@gmail.com", MD5("4002"), "user"),
+    ("Lorena", "lorena@gmail.com", MD5("8922"), "user");
